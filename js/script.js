e1a158945f28b2163e26a3d5ebde15ec754ec822
Davide <kalelc351k@gmail.com>
2020-11-10 00:00:01 +0100

    fixato

--- js/script.js
+++ js/script.js
@@ -6,7 +6,9 @@
 // Possiamo fare anche sullo stesso file o comunque stessa cartella, se ci pensate può essere una sequenza sullo stesso ex.
 // Anche nel punto 2 gli oggetti studenti che stasnno nell’array hanno almeno le 3 proprietà come in quello iniziale.
 
-// - Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. 
+// 1 - Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. 
+console.log('Primo esercizio:');
+console.log(' ');
 
 var studente = {
     'nome': 'Davide',
@@ -14,18 +16,23 @@ var studente = {
     'eta': '33'
 };
 
+console.log(studente);
+console.log(' ');
+
 // Stampare a schermo attraverso il for in tutte le proprietà.
 for (var key in studente) {
    console.log(key + ': ' + studente[key]);
 }
 
-// - Creare un array di oggetti di studenti. 
+// 2 - Creare un array di oggetti di studenti. 
+console.log(' ');
+console.log('Secondo esercizio:');
 
 var arrayStudenti = [
     {
-        'nome': 'Davide',
-        'cognome': 'Zingali',
-        'eta': '33'
+        'nome': 'Danny',
+        'cognome': 'Longo',
+        'eta': '15'
     },
     {
         'nome': 'Sandra',
@@ -46,28 +53,31 @@ var arrayStudenti = [
 
 // Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
 for (var i = 0; i < arrayStudenti.length; i++) {
+    console.log(' ');
+    console.log('Studente: ' + [i+1]);
     for (var key in arrayStudenti[i]) {
             console.log(key + ': ' + arrayStudenti[i][key]);
         }
 }
 
-// - Dare la possibilità all’utente attraverso 3 prompt
-var inputNome = prompt('Inserisci il tuo nome');
-var inputCognome = prompt('Inserisci il tuo cognome');
-var inputEta = prompt("Inserisci la tua eta'");
+// 3 - Dare la possibilità all’utente attraverso 3 prompt
+console.log(' ');
+console.log('Terzo esercizio:');
+console.log(' ');
 
-// di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età. 
+var nome = prompt('Inserisci il tuo nome');
+var cognome = prompt('Inserisci il tuo cognome');
+var eta = prompt("Inserisci la tua eta'");
+
+// Nuovo oggetto studente inserendo nell’ordine: nome, cognome e età. 
 var inputStudente = {
-    'nome': inputNome,
-    'cognome': inputCognome,
-    'eta': inputEta
+    'nome': nome,
+    'cognome': cognome,
+    'eta': eta
 }
+
 console.log(inputStudente);
 
-
-
-
-// Bonus
-// se volete anadate di interazione e/o layout più ricco;
-// e/o gestione casi limite;
-// ciò che volete provare.
+arrayStudenti.push({ nome, cognome, eta });
+console.log(' ');
+console.log(arrayStudenti);
